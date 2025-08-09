1bb81caebe6976b8ab0c8093e06db18f239cfc01
A.Raja <rajaanthonysamy5798@gmail.com>
2025-08-09 21:32:43 +0530

    project init

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+project_name= "cnn_classifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",  ### deployment usiing cicd pipeline
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",  ## Data Ingestion, Data Validation, Data Transformation, Model Training, Model Evaluation
+    f"src/{project_name}/utils/__init__.py",  ## Utility functions for logging, file operations, etc.
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "dvc.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "templates/index.html",
+    ]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logger.info(f"Created directory: {filedir}")
+
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, 'w') as f:
+            pass
+        logger.info(f"Created file: {filepath}")
+    else:
+        logger.info(f"File already exists: {filepath}")
+
+ 
